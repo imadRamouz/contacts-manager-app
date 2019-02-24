2ac73dd42d3b1008b904e8e5e511e2b3845d187f
Imad Ramouz <imadramo@gmail.com>
2019-02-24 21:36:44 +0000

    add the build folder

---
+++ build/precache-manifest.b211e7811e8bf6045231c9d22573a96c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "e4fce9ee66b423429af5",
+    "url": "/static/js/main.e4fce9ee.chunk.js"
+  },
+  {
+    "revision": "325323a9796f6b0ccfcc",
+    "url": "/static/js/2.325323a9.chunk.js"
+  },
+  {
+    "revision": "e4fce9ee66b423429af5",
+    "url": "/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "325323a9796f6b0ccfcc",
+    "url": "/static/css/2.24eeb78b.chunk.css"
+  },
+  {
+    "revision": "d60be0790561cdae1d28456815ceb020",
+    "url": "/index.html"
+  }
+];
