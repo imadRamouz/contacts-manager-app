3825df31cc06b96841bf169a118fdad12566cc62
Imad Ramouz <imadramo@gmail.com>
2019-02-24 21:58:10 +0000

    Updates

---
+++ precache-manifest.69b1465772174c9771305b35a4dc30e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3aeadbcb9e3b3afd4a59",
+    "url": "/contacts-manager-app/static/js/runtime~main.3aeadbcb.js"
+  },
+  {
+    "revision": "714c5b0d5bf9be510e51",
+    "url": "/contacts-manager-app/static/js/main.714c5b0d.chunk.js"
+  },
+  {
+    "revision": "1f06de5af3a999f8f695",
+    "url": "/contacts-manager-app/static/js/2.1f06de5a.chunk.js"
+  },
+  {
+    "revision": "714c5b0d5bf9be510e51",
+    "url": "/contacts-manager-app/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "1f06de5af3a999f8f695",
+    "url": "/contacts-manager-app/static/css/2.24eeb78b.chunk.css"
+  },
+  {
+    "revision": "7d99c0daa1a88b4a1233c3d6f1a050b3",
+    "url": "/contacts-manager-app/index.html"
+  }
+];
